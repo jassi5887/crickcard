150735ba6786b1ec211757a702376d30b8818027
Jatinder Pal Singh <jatinderpalsingh@Jatinders-MacBook-Pro.local>
2018-04-23 23:52:17 +0530

    added missed animation on myprofile

--- src/app/private/main/profile/my-profile/my-profile.component.ts
+++ src/app/private/main/profile/my-profile/my-profile.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { slideUp } from '../../../../shared/animations/slideUp.animation';
 
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
-  styleUrls: ['./my-profile.component.scss']
+  styleUrls: ['./my-profile.component.scss'],
+  animations: [ slideUp ],
+  host: { '[@slideUp]': '' }
 })
 export class MyProfileComponent implements OnInit {
 
