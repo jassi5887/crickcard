4b1ad6ab2db575c77eca8bdf8f33af424842ac39
Jatinder Pal Singh <jatinderpalsingh@Jatinders-MacBook-Pro.local>
2018-05-08 21:50:11 +0530

    added environment variable for API_DOMAIN

--- src/app/services/auth/authentication.service.ts
+++ src/app/services/auth/authentication.service.ts
@@ -3,11 +3,13 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { StorageService } from '../storage/storage.service';
+import { DataService } from '../data/data.service';
 
 @Injectable()
 export class AuthService {
     token: string;
     authenticatedUser;
+    API_DOMAIN;
 
     private httpOptions = {
         headers: new HttpHeaders({
@@ -17,7 +19,10 @@ export class AuthService {
 
     constructor(private router: Router,
                 private http: HttpClient,
-                private storageService: StorageService) {}
+                private storageService: StorageService,
+                private dataService: DataService) {
+        this.API_DOMAIN = this.dataService.getConstant("API_DOMAIN");
+    }
     
 
     isAuthenticated() {
@@ -31,12 +36,12 @@ export class AuthService {
     login(payload, loginStep) {
         switch(loginStep) {
             case "1":
-                return this.http.post('http://localhost:3000/api/otp', payload, this.httpOptions);
+                return this.http.post(`${this.API_DOMAIN}/api/otp`, payload, this.httpOptions);
             case "2":
-                return this.http.post('http://localhost:3000/api/otp/confirm', payload, this.httpOptions);
+                return this.http.post(`${this.API_DOMAIN}/api/otp/confirm`, payload, this.httpOptions);
             case "3":
                 this.httpOptions["observe"] = "response";
-                return this.http.post('http://localhost:3000/api/auth/login', payload, this.httpOptions)
+                return this.http.post(`${this.API_DOMAIN}/api/auth/login`, payload, this.httpOptions)
         }
     }
 
@@ -44,23 +49,23 @@ export class AuthService {
         const token = this.storageService.getAuthToken();
         this.storageService.removeAuthToken();
         const payload = {"x-auth": token};
-        return this.http.post('http://localhost:3000/api/auth/logout', payload, this.httpOptions);
+        return this.http.post(`${this.API_DOMAIN}/api/auth/logout`, payload, this.httpOptions);
     }
 
     register(payload, registrationStep) {
         switch(registrationStep) {
             case "1": 
                 console.log("CASE 1");
-                return this.http.post('http://localhost:3000/api/otp', payload, this.httpOptions);
+                return this.http.post(`${this.API_DOMAIN}/api/otp`, payload, this.httpOptions);
             
             case "2":
                 console.log("CASE 2: ", payload);
-                return this.http.post('http://localhost:3000/api/otp/confirm', payload, this.httpOptions);
+                return this.http.post(`${this.API_DOMAIN}/api/otp/confirm`, payload, this.httpOptions);
             
             case "3":
                 console.log('USER DATA: ', payload);
                 this.httpOptions["observe"] = "response";
-                return this.http.post('http://localhost:3000/api/auth/register', payload, this.httpOptions);
+                return this.http.post(`${this.API_DOMAIN}/api/auth/register`, payload, this.httpOptions);
         }
     }
 }
--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class DataService {
     private userData;
+    private CONSTANTS = {
+        "API_DOMAIN": environment.api_domain
+    };
+
+    getConstant(name) {
+        return this.CONSTANTS[name];
+    }
 
     getUserData() {
         return this.userData;
